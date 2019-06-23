5be0c82b3339e9d1b97f39ada497b1a5c62495fe
Mohammad Mahdi Reisi <mohammad.mahdi.reisi2002@gmail.com>
2019-06-23 19:24:25 +0430

    finish question

--- Football.cpp
+++ Football.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main()
 {
-	cout<<"hello world";
+	int temp=1, mark=0, last;
+	string a;
+	cin>>a;
+	last = a[0];
+	for(int i = 1; i< (int)a.size(); i++)
+	{
+		if(a[i] == last) temp++;
+		else temp = 1, last = a[i];
+		if(temp>=7)
+		{ 
+		mark = 1; 
+		break;
+		}
+	}				
+	if(mark)
+		cout<<"YES";
+	else
+		cout<<"NO";
+
 	return 0;
 }
