e6b86a8baaa933def23bfb01b46dd69b60d4f57a
mohammad mahdi reisi <mohammad.mahdi.reisi2002@gmail.com>
2019-07-03 08:22:49 +0430

    Revert "finish question"
    This reverts commit 5be0c82b3339e9d1b97f39ada497b1a5c62495fe.

--- Football.cpp
+++ Football.cpp
@@ -5,24 +5,6 @@ using namespace std;
 
 int main()
 {
-	int temp=1, mark=0, last;
-	string a;
-	cin>>a;
-	last = a[0];
-	for(int i = 1; i< (int)a.size(); i++)
-	{
-		if(a[i] == last) temp++;
-		else temp = 1, last = a[i];
-		if(temp>=7)
-		{ 
-		mark = 1; 
-		break;
-		}
-	}				
-	if(mark)
-		cout<<"YES";
-	else
-		cout<<"NO";
-
+	cout<<"hello world";
 	return 0;
 }
