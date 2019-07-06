9227b23b84d15f8fc343197617e5465d858590ae
mohammad mahdi reisi <mohammad.mahdi.reisi2002@gmail.com>
2019-07-06 14:53:45 +0430

    add new question

--- Taxi.cpp
+++ Taxi.cpp
@@ -16,6 +16,24 @@ const int MX = 2e5 + 3;
 int main()
 {	
 	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	
+	int n, count[5] = {0, 0, 0, 0, 0}, temp, total=0;
+	cin>>n;	
+	REP(i, 1, n)
+	{
+		cin>>temp;
+		count[temp]++;
+	}
+	total += (count[2]/2) + count[3] + count[4];
+	count[1] -= count[3];
+	if(count[2]%2 == 1)
+	{
+		total++;
+		count[1]-=2;
+	}
+	if(count[1]>0)
+	{
+		total += (count[1]+3) / 4;
+	}
+	cout<<total;
 	return 0;
 }
